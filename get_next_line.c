59bb08264dc351ecf9e9e7535a16961f0023bf0d
Emanuel Castillo Zambrano <emcastil@c2r10s2.42madrid.com>
2024-04-07 16:26:25 +0200

    Doing tests to start over

--- get_next_line.c
+++ get_next_line.c
@@ -10,8 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 0
+
 // Returns a read-line from a fd
 char	*get_next_line(int fd)
 {
+	return (0);
+}
 
+int	main(void)
+{
+	int		fd;
+	size_t	i;
+	char	buffer[BUFFER_SIZE];
+	ssize_t	bytes_read;
+	int		found_newline = 0;
+
+	fd = open("file.ecz", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error at opening.");
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read == - 1)
+	{
+		perror("Error at reading file.");
+		close(fd);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0';
+
+	printf("%s", buffer);
+
+	close(fd);
+	return (0);
 }
