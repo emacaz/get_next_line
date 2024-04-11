90c89e6053a89f01425b236a8eb0a6f117ce9c6e
Emanuel Castillo Zambrano <emcastil@c2r13s2.42madrid.com>
2024-04-11 19:54:27 +0200

    No reading file

--- get_next_line.c
+++ get_next_line.c
@@ -15,49 +15,59 @@
 // Receives a fd and returns the length of bytes
 size_t	read_file(int fd)
 {
-	
+	ssize_t	bytes_read;
+	ssize_t	total_length;
+	char	*total_char;
+
+	total_length = 0;
+	total_char = NULL;
+	while ((bytes_read = read(fd, total_char, BUFFER_SIZE)) > 0)
+		total_length += bytes_read;
+	if (bytes_read == -1)
+		total_length = -1;
+	return (total_length);
 }
 
 // Returns a read-line from a fd
-char	*get_next_line(int fd)
-{
-	size_t	db_length;
+// char	*get_next_line(int fd)
+// {
+// 	size_t	db_length;
 
-	db_length = read_file(fd);
+// 	db_length = read_file(fd);
 
-	size_t			i;
-	ssize_t			bytes_read;
-	static char		*database;
+// 	size_t			i;
+// 	ssize_t			bytes_read;
+// 	static char		*database;
 
-	database = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	// I can't allocate BUFER_SIZE. What if is 1?
-	if (!database)
-		return (NULL);
-	i = 0;
-	while (i < BUFFER_SIZE)
-	{
-		bytes_read = read(fd, database, BUFFER_SIZE);
-		if (bytes_read == -1)
-		{
-			free(database);
-			return (NULL);
-		}
-		if (database[i] == '\n')
-		{
-			i++;
-			break ;
-		}
-		i++;
-	}
-	database[i] = '\0';
-	return (database);
-}
+// 	database = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+// 	// I can't allocate BUFER_SIZE. What if is 1?
+// 	if (!database)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < BUFFER_SIZE)
+// 	{
+// 		bytes_read = read(fd, database, BUFFER_SIZE);
+// 		if (bytes_read == -1)
+// 		{
+// 			free(database);
+// 			return (NULL);
+// 		}
+// 		if (database[i] == '\n')
+// 		{
+// 			i++;
+// 			break ;
+// 		}
+// 		i++;
+// 	}
+// 	database[i] = '\0';
+// 	return (database);
+// }
 
 int	main(int argc, char *argv[])
 {
 	int		fd;
-	char	*next_line;
-	char	*next_line2;
+	// char	*next_line2;
+	ssize_t	file_length;
 
 	if (argc == 2)
 	{
@@ -69,26 +79,22 @@ int	main(int argc, char *argv[])
 		fd = STDIN_FILENO;
 	else
 		return (printf("Error here"), 1);
-	next_line = get_next_line(fd);
-	if (next_line)
+	file_length = read_file(fd);
+	if (file_length)
 	{
-		printf("%s", next_line);
-		free(next_line);
+		printf("%zd", file_length);
+		// free(next_line);
 	}
 	if (argc == 2)
 		close(fd);
 
-	next_line2 = get_next_line(fd);
-	if (next_line2)
-	{
-		printf("%s", next_line2);
-		free(next_line2);
-	}
+	// next_line2 = get_next_line(fd);
+	// if (next_line2)
+	// {
+	// 	printf("%s", next_line2);
+	// 	free(next_line2);
+	// }
 	if (argc == 2)
 		close(fd);
 	return (0);
 }
-
-// Receives fd
-// read file with BUFFER_SIZE
-// Divide till find '\n' 1.Return 2.Rest (if there is)
