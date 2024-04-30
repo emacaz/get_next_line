79015fe1fba8e6b0c8b9287ffe1b7392d39ca574
Emanuel Castillo <ecastillo76@misena.edu.co>
2024-04-30 21:42:37 +0200

    Apparently first part working...

--- get_next_line.c
+++ get_next_line.c
@@ -5,96 +5,150 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/06 15:28:26 by emcastil          #+#    #+#             */
-/*   Updated: 2024/04/06 15:28:30 by emcastil         ###   ########.fr       */
+/*   Created: 2024/04/26 16:34:25 by emcastil          #+#    #+#             */
+/*   Updated: 2024/04/28 17:16:42 by emcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h" 
+#include "get_next_line.h"
 
-// Receives a fd and returns the length of bytes
-size_t	read_file(int fd)
+/*
+ * Rearrange chain for next call
+*/
+void	arrange_chain(t_list **chain)
 {
-	ssize_t	bytes_read;
-	ssize_t	total_length;
-	char	*total_char;
+	t_list	*last_node;
+	t_list	*empty_node;
+	int		i;
+	int		k;
+	char	*buf;
 
-	total_length = 0;
-	total_char = NULL;
-	while ((bytes_read = read(fd, total_char, BUFFER_SIZE)) > 0)
-		total_length += bytes_read;
-	if (bytes_read == -1)
-		total_length = -1;
-	return (total_length);
+	buf = malloc(BUFFER_SIZE + 1);
+	empty_node = malloc(sizeof(t_list));
+	if (NULL == buf || NULL == empty_node)
+		return ;
+	last_node = find_last_node(*chain);
+	i = 0;
+	k = 0;
+	while (last_node->str_buf[i] && last_node->str_buf[i] != '\n')
+		++i;
+	while (last_node->str_buf[i] && last_node->str_buf[++i])
+		buf[k++] = last_node->str_buf[i];
+	buf[k] = '\0';
+	empty_node->str_buf = buf;
+	empty_node->next = NULL;
+	free_chain(chain, empty_node, buf);
 }
 
-// Returns a read-line from a fd
-// char	*get_next_line(int fd)
-// {
-// 	size_t	db_length;
-
-// 	db_length = read_file(fd);
-
-// 	size_t			i;
-// 	ssize_t			bytes_read;
-// 	static char		*database;
-
-// 	database = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-// 	// I can't allocate BUFER_SIZE. What if is 1?
-// 	if (!database)
-// 		return (NULL);
-// 	i = 0;
-// 	while (i < BUFFER_SIZE)
-// 	{
-// 		bytes_read = read(fd, database, BUFFER_SIZE);
-// 		if (bytes_read == -1)
-// 		{
-// 			free(database);
-// 			return (NULL);
-// 		}
-// 		if (database[i] == '\n')
-// 		{
-// 			i++;
-// 			break ;
-// 		}
-// 		i++;
-// 	}
-// 	database[i] = '\0';
-// 	return (database);
-// }
-
-int	main(int argc, char *argv[])
+/*
+* Extracts and returns the first line up to
+* a newline character from a linked list of strings.
+*/
+char	*get_line(t_list *chain)
 {
-	int		fd;
-	// char	*next_line2;
-	ssize_t	file_length;
+	char	*next_string;
+	int		s_len;
 
-	if (argc == 2)
+	if (NULL == chain)
+		return (NULL);
+	s_len = len_to_newline(chain);
+	next_string = (char *)malloc(s_len + 1);
+	if (NULL == next_string)
+		return (NULL);
+	copy_string(chain, next_string);
+	return (next_string);
+}
+
+/*
+* Adds a new node with provided data to the end of a linked list.
+*/
+void	chain_add_back(t_list **chain, char *line)
+{
+	t_list	*new_node;
+	t_list	*temp;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	new_node->str_buf = line;
+	new_node->next = NULL;
+	if (*chain == NULL)
 	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-			return (printf("Error at opening file"), 1);
+		*chain = new_node;
 	}
-	else if (argc == 1)
-		fd = STDIN_FILENO;
 	else
-		return (printf("Error here"), 1);
-	file_length = read_file(fd);
-	if (file_length)
 	{
-		printf("%zd", file_length);
-		// free(next_line);
+		temp = *chain;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
 	}
-	if (argc == 2)
-		close(fd);
-
-	// next_line2 = get_next_line(fd);
-	// if (next_line2)
-	// {
-	// 	printf("%s", next_line2);
-	// 	free(next_line2);
-	// }
-	if (argc == 2)
-		close(fd);
-	return (0);
 }
+/*
+* Fills a linked list with data from a file descriptor
+* until encountering a newline or end of data.
+*/
+void	create_chain(t_list **chain, int fd)
+{
+	char	*buffer;
+	int		bytes_read;
+
+	while (!is_newline(*chain))
+	{
+		buffer = (char *)malloc(BUFFER_SIZE + 1);
+		if (!buffer)
+			return ;
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (!bytes_read)
+		{
+			free(buffer);
+			return ;
+		}
+		buffer[bytes_read] = '\0';
+		chain_add_back(chain, buffer);
+	}
+}
+
+// Receives a fd and returns every \n-ended line
+char	*get_next_line(int fd)
+{
+	static t_list	*chain = NULL;
+	char			*line;
+
+	if (fd == -1 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
+		return (NULL);
+	create_chain(&chain, fd);
+	if (chain == NULL)
+		return (NULL);
+	line = get_line(chain);
+	arrange_chain(&chain);
+	return (line);
+}
+
+/* ****************************** */
+int main(int argc, char *argv[])
+{
+    int fd;
+    char *line;
+
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    // Open the file specified by the user
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 1;
+    }
+    while ((line = get_next_line(fd)) != NULL) {
+        printf("%s", line);
+        free(line); 
+    }
+
+    // Close the file descriptor
+    close(fd);
+    return 0;
+}
+/* ****************************** */
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: emcastil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 15:28:38 by emcastil          #+#    #+#             */
-/*   Updated: 2024/04/06 15:28:40 by emcastil         ###   ########.fr       */
+/*   Updated: 2024/04/28 16:42:07 by emcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
+#  define BUFFER_SIZE 42
 # endif
 
 # include <fcntl.h>
@@ -22,6 +22,20 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}					t_list;
+
 char	*get_next_line(int fd);
+void	create_chain(t_list **chain, int fd);
+void	chain_add_back(t_list **chain, char *line);
+int		is_newline(t_list *chain);
+int		len_to_newline(t_list *list);
+void	copy_string(t_list *list, char *str);
+void	arrange_chain(t_list **chain);
+t_list	*find_last_node(t_list *chain);
+void	free_chain(t_list **chain, t_list *empty_node, char *buffer);
 
 #endif
